c9142ccd4db6fa824690c2eb577b1ea416b3941a
Clifton Rawlings <Clifton.Raw@gmail.com>
2020-10-04 17:34:38 -0700

    TCP Server Testing

--- main.cpp
+++ main.cpp
@@ -1,14 +1,49 @@
 #include <iostream>
-#include "Game.hpp"
+#include <cstring>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <arpa/inet.h>
 
 int main(int argc, char* argv[])
 {
-    Game game = Game();
-    Game().Run();
+    int status;
+    char ip[INET_ADDRSTRLEN];
+    struct addrinfo addr;
+    struct addrinfo *s, *res;
 
-    cout << "Game has terminated successfully, score: "
-         << game.GetScore() << ", size: " << game.GetSize()
-         << endl;
+    if (argc != 2)
+    {
+        fprintf(stderr, "Include port number");
+        return 1;
+    }
+    
+    // Set addr struct to 0
+    memset(&addr, 0, sizeof(addr));
+
+    addr.ai_family      = AF_INET;      // IP4
+    addr.ai_socktype    = SOCK_STREAM;  // TCP Stream
+    addr.ai_flags       = AI_PASSIVE;   // Use Current IP of Host
+
+    // Sets up structure for HOST IP address
+    status = getaddrinfo(NULL, argv[1], &addr, &res);
+    //status = getaddrinfo(argv[1], NULL, &addr, &res);
+
+    if (status != 0)
+    {
+        fprintf(stderr, "An error has occurred: %s\n", gai_strerror(status));
+        exit(EXIT_FAILURE);
+    }
+    
+    for (s = res; s != NULL; s = s->ai_next)
+    {
+        int fd = socket(addr.ai_family, addr.ai_socktype, addr.ai_protocol);
+        printf("%d", s->ai_protocol);
+    }
+    
+
+    //printf("%s", ip);
 
     return 0;
 }
