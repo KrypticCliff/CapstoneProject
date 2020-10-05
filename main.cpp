05b0c7fca897110880e503c4c18337ed503327f2
Clifton Rawlings <Clifton.Raw@gmail.com>
2020-10-05 00:11:32 -0700

    Setup server-side app

--- main.cpp
+++ main.cpp
@@ -9,41 +9,54 @@
 int main(int argc, char* argv[])
 {
     int status;
+    int sfd;
     char ip[INET_ADDRSTRLEN];
-    struct addrinfo addr;
-    struct addrinfo *s, *res;
+    struct addrinfo addr, *res;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "Include port number");
-        return 1;
-    }
-    
     // Set addr struct to 0
     memset(&addr, 0, sizeof(addr));
-
     addr.ai_family      = AF_INET;      // IP4
     addr.ai_socktype    = SOCK_STREAM;  // TCP Stream
     addr.ai_flags       = AI_PASSIVE;   // Use Current IP of Host
 
     // Sets up structure for HOST IP address
-    status = getaddrinfo(NULL, argv[1], &addr, &res);
-    //status = getaddrinfo(argv[1], NULL, &addr, &res);
-
+    status = getaddrinfo(NULL, "8333", &addr, &res);
+    
     if (status != 0)
     {
         fprintf(stderr, "An error has occurred: %s\n", gai_strerror(status));
         exit(EXIT_FAILURE);
     }
-    
+
+    sfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+
+    bind(sfd, res->ai_addr, res->ai_addrlen);
+
+    // Creates Socket File Descriptor from Addr setup
+    //int sfd = socket(addr.ai_family, addr.ai_socktype, addr.ai_protocol);
+
+    // Checks if there are more IP addresses connected to this host (DNS Lookup)    
+
+
+/*
     for (s = res; s != NULL; s = s->ai_next)
     {
-        int fd = socket(addr.ai_family, addr.ai_socktype, addr.ai_protocol);
-        printf("%d", s->ai_protocol);
+        int sfd = socket(s->ai_family, s->ai_socktype, s->ai_protocol);
+        struct sockaddr_in *saddr = (struct sockaddr_in *) s->ai_addr;
+        int port = ntohs(saddr->sin_port);
+
+        inet_ntop(res->ai_family, &saddr->sin_addr, ip, INET_ADDRSTRLEN);
+
+        if (sfd < 0)
+        {
+            fprintf(stderr, "An Error Has Occurred with the File Descriptor");
+        }
+
+        printf("Current Setup:\nIP:%s\nPort:%d\n", ip, port);
     }
+*/
+    //while (true)    
     
-
-    //printf("%s", ip);
-
+    freeaddrinfo(res);
     return 0;
 }
