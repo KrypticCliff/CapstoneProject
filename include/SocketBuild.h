637b2eb716eedc20ea5a3ec15e3b9a035fbffa4e
Clifton Rawlings <Clifton.Raw@gmail.com>
2020-10-13 00:45:45 -0700

    Added AcceptSocket to include

--- include/SocketBuild.h
+++ include/SocketBuild.h
@@ -5,8 +5,10 @@
 #include <cstddef>
 
 int CreateSocket(addrinfo &hint, addrinfo *&res, int flag, const char* node, const char* service);
+int AcceptSocket(int sfd, sockaddr_storage c_addr);
 void ConnectToServer(int sfd, addrinfo *&res);
 size_t SendMessage(int sfd, const char* msg, size_t msize);
+
 //int RecvMessage(int socket, char*& buf, size_t bsize);
 
 #endif
