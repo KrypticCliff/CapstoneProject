2b9c1c95b3c98df6ae466f6e6f6f81a467872cb4
Cliff Rawlings <crawlings@google.com>
2020-10-12 19:51:35 -0700

    Added test client

--- client.cpp
+++ client.cpp
@@ -10,6 +10,8 @@
 #include <netdb.h>
 #include <unistd.h>
 
+#include "./include/SocketBuild.h"
+
 #define LOCALPORT       "8333"
 #define MAXDATASIZE     100
 
@@ -24,21 +26,24 @@ int main(int argc, char* argv[])
     socklen_t addrlen;
     fd_set readfd;
 
+/*
     hint.ai_flags    = 0;
     hint.ai_family   = AF_INET;
     hint.ai_socktype = SOCK_STREAM;
-
+*/
     if (argc != 3)
     {
-        printf("Address and Port Needed. <IP> <Port>");
+        printf("Address and Port Needed. <IP> <Port>\n");
         return -1;
     }
-    
+
+    sfd = CreateSocket(hint, res, 0, argv[1], argv[2]);
+/*  
     // Set hint with 0 bytes
     memset(&hint, 0, sizeof(hint));
 
     // Setup addrinfo struct with user-inputted server info
-    if ((status = getaddrinfo(argv[1], argv[2], &hint, &res)) != 0)
+    if ((status = getaddrinfo(argv[1], LOCALPORT, &hint, &res)) != 0)
     {
         fprintf(stderr, "getaddrinfo Error: %s\n", gai_strerror(status));
         exit(EXIT_FAILURE);
@@ -46,15 +51,17 @@ int main(int argc, char* argv[])
 
     // Establish Socket File Descriptor
     sfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-
+*/
     // Connect to Server
+    ConnectToServer(sfd, res);
+/*
     if (connect(sfd, res->ai_addr, res->ai_addrlen) == -1)
     {
         close(sfd);
         perror("Connect Failed:");
         exit(EXIT_FAILURE);
     }
-
+*/
     printf("You are now connected to ... You may now chat!\n");
 
     while(1)
@@ -63,28 +70,16 @@ int main(int argc, char* argv[])
         char msg[50];
         fgets(msg, 50, stdin);  // Data to STDIN
 
-        /*
-        retval = send(sfd, msg, 50, 0);
-        
-        if (retval == -1)
-        {
-            perror("Send Fail:");
-            continue;
-        }
-        else if (retval == 0)
-        {
-            perror("Send Disconnect:");
-            exit(EXIT_SUCCESS);
-        }
-        */
-
         // Send currently blocking. 
         // Takes up to 50 char per message sent
+        retval = SendMessage(sfd, msg, 50);
+    /*
         if (send(sfd, msg, 50, 0) == -1)
         {
             perror("Send Fail:");
             continue;
         }
+    */
     }
     close(sfd);
 }
